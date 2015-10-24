352ac6604e3ae4ffc77c44d5fd9acbf3ccd9055e
seansaito <saitosean@ymail.com>
2015-10-25 00:32:03 +0800

    Fixed dictionary key mismatch in global_vars.py

--- global_vars.py
+++ global_vars.py
@@ -14,14 +14,14 @@ script_dir = os.path.dirname(__file__)
 rel_paths = {
     "top10_bandwidth": "app/static/json/top10_bandwidth.json",
     "top10_consensus": "app/static/json/top10_consensus.json",
-    "all_families": "app/static/json/all.json"
+    "all": "app/static/json/all.json"
 }
 
 # Absolute paths
 abs_paths = {
     "top10_bandwidth": os.path.join(script_dir, rel_paths["top10_bandwidth"]),
     "top10_consensus": os.path.join(script_dir, rel_paths["top10_consensus"]),
-    "all_families": os.path.join(script_dir, rel_paths["all_families"])
+    "all": os.path.join(script_dir, rel_paths["all"])
 }
 
 # Flags for the relays
